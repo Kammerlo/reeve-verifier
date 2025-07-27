ff8c7dc95ed5bb3f4b4f45a19ab814bdff9a3f1c
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-07-27 12:34:14 +0200

    chore: removed cross origin check

--- src/main/java/org/cardanofoundation/reeve/indexer/resource/TransactionController.java
+++ src/main/java/org/cardanofoundation/reeve/indexer/resource/TransactionController.java
@@ -12,7 +12,6 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
